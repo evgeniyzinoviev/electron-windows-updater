3957ee76da80eec2162bf8666abc1021b42cfaf6
evgeny <me@ch1p.com>
2016-08-31 19:39:00 +0300

    upd

--- copy-elevated.bat
+++ copy-elevated.bat
@@ -9,11 +9,9 @@ ping -n 2 127.0.0.1
 xcopy /e /y /i %src% %dst%
 rmdir /s /q %src%
 
-for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
-if "%version%" == "10.0" (
-  start "" %sysroot%\ie4uinit.exe -show
-) else (
+if [%sysroot%] NEQ [] (
   start "" %sysroot%\ie4uinit.exe -ClearIconCache
+  start "" %sysroot%\ie4uinit.exe -show
 )
 
 start "" %exe%
